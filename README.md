d1c88f3226683710cc65c344760509a577271856
ro <RobelH@outlook.com>
2022-11-29 15:14:29 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# react-boilerplate
+<<<<<<< HEAD
+
+ Basic front-end template for building web applications with React.
+
+=======
+Starter react boilerplate.
+>>>>>>> 34bcc0d26e8fc8029824b86e1870df3f5be5c065
---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+ import React, { Component} from "react";
+
+ class App extends Component {
+
+    render(){
+        return(
+            <div>
+                <h2>React starter config</h2>
+            </div>
+        )
+    }
+ }
+
+ export default App;
